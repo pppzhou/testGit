58aaa16350421c5a3cb2de1f2b939c9141453079
zhoupeng <zhoupeng@mininglamp.com>
2020-03-31 14:05:20 +0800

    merge code8 and code 9

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -11,5 +11,6 @@ public class HelloWorld {
         System.out.println("test merge code4");
         System.out.println("test merge code7");
         System.out.println("test merge code6");
+        System.out.println("test merge code9");
     }
 }
